fdb021116ad145481ffe1eff18a1e4215dc4231f
leonard <40259905+leonard246@users.noreply.github.com>
2019-08-13 20:51:12 +1000

    resolving correctly

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var ozbd = require('../functions');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('Response')
-  console.log(ozbd);  
-  res.send(ozbd);
+  res.send("users page");
 });
 
 module.exports = router;
---
+++ urls.js
@@ -0,0 +1,7 @@
+const _OZBARGAIN_URLS = [ 'https://www.ozbargain.com.au/',
+                        'https://www.ozbargain.com.au/?page=1',
+                        'https://www.ozbargain.com.au/?page=2',
+                        ]
+
+
+module.exports = _OZBARGAIN_URLS;

5b89708677f2e4d4763b9fba7b4fb0d65e31b7d9
leonard <40259905+leonard246@users.noreply.github.com>
2019-08-13 21:28:32 +1000

    react working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = { data: [] };
+
+  componentDidMount() {
+    fetch(`/ozb`)
+    .then(res => res.json())
+    .then(data => this.setState({ data }));
+  }
+  
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data</h1>
+      <ul>
+        {this.state.data.map( d =>
+          <li>
+            {d.title}<br/>
+            {d.submitDetail}<br/>
+            <a href={d.link}>{d.link}</a><br/>
+          </li>
+        )}
+      </ul> 
     </div>
-  );
+  )}
 }
 
 export default App;
---
+++ routes/ozb.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+const cheerio = require('cheerio');
+var URLS = require('../urls')
+
+let dataArr = []; 
+
+function scrapeOzb(url) {
+  request.get(url, (err, res, html) => {
+    if(err) {console.log(`There's an error with url --> ${url}`)}
+  
+    else {
+      let $ = cheerio.load(html);
+      let nodes = [], objectNodes = []; 
+      
+      $('div.node').map((i,e) => { nodes.push(e.attribs.id); });
+      nodes = nodes.filter(e => { return e!==undefined ? true:false; });
+
+      nodes.forEach((e,i) => { 
+        objectNodes[i] = {
+          title: e.replace('node','h2#title'),
+          submitDetail: $(e.replace('node','h2#title')).next().text(),
+          link: e.replace('node',`https://www.ozbargain.com.au/node/`),
+        };
+      });
+        
+      // remove expired and out of stock deals
+      objectNodes = objectNodes.filter(e => {
+        if($(e.title).text().search('expired') > -1 || $(e.title).text().search('out of stock') > -1) 
+          return false;
+        else return true;
+      });
+
+      dataArr = [...objectNodes];
+      dataArr.forEach(e => { e.title = $(e.title).text().trim(); })
+    }
+  });
+}
+scrapeOzb(URLS[0]);
+
+/* Get Ozb page. */
+router.get('/', function(req, res, next) {
+  res.send(dataArr);
+});
+
+module.exports = router;

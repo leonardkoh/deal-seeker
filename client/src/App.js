78b28ce596857a13355b402004361c8af249cbb1
leonard <40259905+leonard246@users.noreply.github.com>
2019-08-14 20:39:26 +1000

    component display button added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import './App.css';
+
+class ReactButtonDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { showDetails: "false" }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ showDetails: "true"})
+  }
+
+  render() {
+    return (<div>state: {this.state.showDetails}</div>);
+    
+    // return (<button>{this.props.LinkInfo}</button>);
+  }
+}
 
 class App extends React.Component {
-  state = { data: [] };
+  constructor(props) {
+    super(props);
+
+    this.state = { data: [],
+                   showDetails: false,
+                   showComments: false
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
 
   componentDidMount() {
     fetch(`/ozb`)
@@ -10,16 +38,25 @@ class App extends React.Component {
     .then(data => this.setState({ data }));
   }
   
+  onClick(e) {
+    // this.setState({ showDetails: true})
+    console.log(e.target.value);
+  } 
+
   render() {
     return (
     <div className="App">
       <h1>Data</h1>
       <ul>
-        {this.state.data.map( d =>
+        {this.state.data.map(data =>
           <li>
-            {d.title}<br/>
-            {d.submitDetail}<br/>
-            <a href={d.link}>{d.link}</a><br/>
+            <h3>{data.title}</h3>
+            <p className="font-italic">{data.submitDetail}</p>
+            <a href={data.link} className="btn btn-outline-primary btn-sm" value="DETAILS">DETAILS</a>
+            <a href={data.link} className="btn btn-outline-primary btn-sm" value="COMMENTS">COMMENTS</a>
+            <button className="btn btn-outline-secondary btn-sm" value={data.link} onClick={this.onClick}>TOGGLE</button>
+            <br/>
+            <ReactButtonDetails LinkInfo={data.link} showDetails={this.state.showDetails}/>
           </li>
         )}
       </ul> 

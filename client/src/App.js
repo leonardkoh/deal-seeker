c6ba10db0c83abdfbf537b8bf942ee81271e106b
leonard <40259905+leonard246@users.noreply.github.com>
2019-08-18 17:22:58 +1000

    styling added

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,15 @@ class ReactDetailsButton extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    // this.setState({showDetails: true})
-    // if(this.state.showDetails)
-    //   console.log(`props: ${this.props.LinkInfo}`);
+  componentDidMount() {
     fetch('/ozbnode')
     .then(res => res.json())
-    .then(nd => this.setState({ nodeData: nd }))
-    // console.log(`Button Value: ${this.props.LinkInfo}`);
+    .then(nd => this.setState({ nodeData: nd }));
+  }
+  
+  handleClick() {
+    this.setState({showDetails: true});
     console.log(this.state.nodeData);
-    // this.setState(fetch('/ozbnode'));
   }
 
   render() {
@@ -47,18 +46,47 @@ class App extends React.Component {
 
   render() {
     return (
-    <div className="App">
+    <div className="app">
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Deal Seeker</a> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Ozbargain <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link 2</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link 3</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <h1>Ozbargain Deals</h1>
       <ul>
         {this.state.data.map((data,i) =>
-          <li key={i}>
-            <h3>{data.title}</h3>
-            <h6 className="font-italic">{data.submitDetail}</h6>
-                <ReactDetailsButton LinkInfo={data.link}/>
+            <li key={i}>
+              <div className="row">
+                <div className="col-xs">
+                <img src="https://sgwlawfirm.com/wp-content/plugins/lawyer-helper/assets/img/placeholder-category.png" alt="deal-node" height="50vw" width="50vw"/>
+                </div>
+                <div className="col">
+                  <h3>{data.title}</h3>
+                  <h6 className="font-italic">{data.submitDetail}</h6>
+                  <ReactDetailsButton LinkInfo={data.link}/>
+                </div>
+              </div>
             <br/>
           </li>
         )}
-      </ul> 
+      </ul>
     </div>
   )}
 }
--- routes/ozbnode.js
+++ routes/ozbnode.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 const cheerio = require('cheerio');
 
-let dataArr; 
+let dataArr = []; 
 TEST_URL = 'https://www.ozbargain.com.au/node/477203';
 
 function scrapeNode() {
@@ -13,29 +13,8 @@ function scrapeNode() {
     else {
       let $ = cheerio.load(html);
       let nodes = [], objectNodes = []; 
-      dataArr = $('ul div.content').text();
-      // $('div.node').map((i,e) => { nodes.push(e.attribs.id); });
-      // nodes = nodes.filter(e => { return e!==undefined ? true:false; });
-
-//       nodes.forEach((e,i) => { 
-//         objectNodes[i] = {
-//           title: e.replace('node','h2#title'),
-//           submitDetail: $(e.replace('node','h2#title')).next().text(),
-//           link: e.replace('node',`https://www.ozbargain.com.au/node/`),
-//         };
-//       });
-        
-//       // remove expired and out of stock deals
-//       objectNodes = objectNodes.filter(e => {
-//         if($(e.title).text().search('expired') > -1 || $(e.title).text().search('out of stock') > -1) 
-//           return false;
-//         else return true;
-//       });
-
-//       dataArr = [...objectNodes];
-//       dataArr.forEach(e => { e.title = $(e.title).text().trim(); })
-//     }
-//   });
+      dataArr[0] = $('ul div.content').text();
+      // dataArr = [1,2,3];
 }})}
 
 scrapeNode();
